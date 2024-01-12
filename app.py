ff90cc26ab7dc60fd82bd6cb047fca18dada0735
George Thiel <georgethiel@Georges-MacBook-Pro.local>
2024-01-12 11:50:14 +0100

    guess

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
-import calculator
-import hondenfeiten
-import telletter
-import grap
+import guess
+# import hondenfeiten
+# import telletter
+# import grap
 
 app = Flask(__name__)
 CORS(app)
@@ -14,19 +14,19 @@ CORS(app)
 def hello_world():
     return "<p>Hij doet  het YC 2401</p>"
 
-@app.route("/calculator")
-def calculatorfunc():
-    return calculator.functie_een()
+@app.route("/guess")
+def guessfunc():
+    return guess.functie_een()
 
-@app.route("/hondenfeiten")
-def hondenfeitenfunc():
-    return hondenfeiten.functie_een()
+# @app.route("/hondenfeiten")
+# def hondenfeitenfunc():
+#     return hondenfeiten.functie_een()
 
 
-@app.route("/telletter")
-def telletterfunc():
-    return telletter.functie_een()   
+# @app.route("/telletter")
+# def telletterfunc():
+#     return telletter.functie_een()   
 
-@app.route("/grap")
-def grapfunc():
-    return grap.functie_een()             
+# @app.route("/grap")
+# def grapfunc():
+#     return grap.functie_een()             
--- calculator.py
+++
@@ -1,4 +0,0 @@
-def functie_een():
-    input_user = int(input("vul een getal: "))
-    output_user = input_user + 5
-    return str(output_user)
