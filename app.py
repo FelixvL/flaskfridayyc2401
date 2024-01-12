3a35b192f752663c45114ed546a8c38418321273
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-01-12 11:09:50 +0100

    go

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
+import calculator
+import hondenfeiten
+import telletter
+import grap
+
 app = Flask(__name__)
 CORS(app)
 
@@ -10,8 +15,18 @@ def hello_world():
     return "<p>Hij doet  het YC 2401</p>"
 
 @app.route("/calculator")
-def calculator():
-    input_user = int(input("vul een getal: "))
-    output_user = input_user + 5
-    return str(output_user)
-                     
+def calculatorfunc():
+    return calculator.functie_een()
+
+@app.route("/hondenfeiten")
+def hondenfeitenfunc():
+    return hondenfeiten.functie_een()
+
+
+@app.route("/telletter")
+def telletterfunc():
+    return telletter.functie_een()   
+
+@app.route("/grap")
+def grapfunc():
+    return grap.functie_een()             
