aafcb5473b9da289eb60b7817d7f29419164a698
Mar Fernández <mmffa1996@gmail.com>
2024-01-12 15:05:24 +0100

    eerste commit

---
+++ balletje.py
@@ -0,0 +1,29 @@
+print("Raad de balletje")
+from random import shuffle
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+
+mylist = [' ', 'O', ' ']
+shuffle_list(mylist)
+print(mylist)
+
+def player_guess():
+
+    guess=''
+
+    while guess not in ['0', '1', '2']:
+        guess = input("Pick a number: 0, 1, or 2")
+    if guess == '0':
+        print("Balletje gevonden")
+    else:
+        print("Balletje niet gevonden")
+
+    return int(guess)
+
+print(player_guess())
+
+    
+
